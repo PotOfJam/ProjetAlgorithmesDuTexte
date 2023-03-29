585d393fefa5dbf9701a322fc320f1eb06f1c8bd
bonnail <julie.bonnail@etu.unistra.fr>
2023-03-29 15:54:27 +0200

    interface

--- src/main.py
+++ src/main.py
@@ -4,20 +4,10 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from app.application import Application
 
 
-def action_btn():
-    print("Ok")
-
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Application()
-
-    qBtn = QPushButton(window)
-    qBtn.setText("Démarrer")
-    qBtn.setGeometry(100, 100, 200, 30)
-    qBtn.clicked.connect(action_btn)
-    window.show()
 
     app.exec_()
